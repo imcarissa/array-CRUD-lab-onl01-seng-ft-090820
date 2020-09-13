12f9d0d515ce9f0392550f573050a508e0a6c29b
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-13 23:43:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = create_an_array
-  create_an_array.pop("arrays!")
+  array = create_an_array.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
