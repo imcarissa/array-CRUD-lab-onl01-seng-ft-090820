bd07f83c255ed74450ae45be79b9b753aa58f008
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-13 22:43:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  friends << ross
-  puts friends.inspect
+  friends << "ross"
 end
 
 def add_element_to_start_of_array(array, element)
