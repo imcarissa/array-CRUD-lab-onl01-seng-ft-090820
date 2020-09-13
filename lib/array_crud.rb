99f7182c9c835b1278c1216c5b7226bde6c67c31
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-13 22:50:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   friends = ["rachel", "joey", "monica", "chandler"]
-  friends.push("ross")
+  puts friends.push("ross")
 end
 
 def add_element_to_start_of_array(array, element)
